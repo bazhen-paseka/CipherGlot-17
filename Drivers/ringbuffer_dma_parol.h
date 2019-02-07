1dab3c910357590cc1c2e615fd9ddd6cbec72db1
Бажен Левковець <bazhen.paseka@gmail.com>
2019-02-07 12:39:07 +0200

    to stash
    stash 2
    
    
    .

---
+++ Drivers/ringbuffer_dma_parol.h
@@ -0,0 +1,17 @@
+#ifndef RINGBUFFER_DMA_PAROL_H_INCLUDED
+#define RINGBUFFER_DMA_PAROL_H_INCLUDED
+
+	/* ESP8266 variables */
+	#define THINGSPEAK_ADDRESS	"api.thingspeak.com"
+	#define THINGSPEAK_API_KEY	"5ZRXVI7HIJQPGKID"				// Enter your Write API here
+
+//	#define WIFI_SSID			"Kitchen"
+//	#define WIFI_PASS			"Papanina36"
+//
+//	#define WIFI_SSID			"Tapac"
+//	#define WIFI_PASS			"27051329"
+
+	#define WIFI_SSID			"Redmi"
+	#define WIFI_PASS			"16251117"
+
+#endif
