f6cc5e5e744d533539adfde0fca23fcd22636b42
Бажен Левковець <bazhen.paseka@gmail.com>
2020-07-13 20:08:56 +0300

    монотонне бікання, друк часу, локальний конфіг

---
+++ Drivers/Local_config/cipherglot_local_config.h
@@ -0,0 +1,11 @@
+
+#ifndef CIPHERGLOT17_LOCAL_CONFIG_H_INCLUDED
+#define CIPHERGLOT17_LOCAL_CONFIG_H_INCLUDED
+
+//**********************************************************************
+
+#define SOFT_VERSION 	170
+
+//**********************************************************************
+
+#endif
